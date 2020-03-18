2d06543c71a51e85ae303522c1ade6313aa292a0
Arthit Kanjai <arthit.kanjai@nxp.com>
2020-03-18 11:12:04 +0700

    test

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4445/wd/hub',
+  seleniumAddress: 'http://133b0901b280:4445/wd/hub',
 
   // add more browsers to this array for more parallel tests!!
   multiCapabilities: [

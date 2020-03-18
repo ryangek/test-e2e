0a82251d172b7bb2acae88186b07d6223ee10d31
Arthit Kanjai <arthit.kanjai@nxp.com>
2020-03-18 13:10:18 +0700

    save

--- conf.js
+++ conf.js
@@ -1,18 +1,17 @@
 exports.config = {
-  seleniumAddress: 'http://133b0901b280:4445/wd/hub',
+  // seleniumAddress: 'http://133b0901b280:4445/wd/hub',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
 
   // add more browsers to this array for more parallel tests!!
   multiCapabilities: [
     {
       browserName: 'chrome',
-      chromeOptions: {
-        binary: 'node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_80.0.3987.106.exe',
-        args: ['--start-maximized', 'disable-infobars']
-      }
+      seleniumAddress: 'http://10.105.178.758:5555/wd/hub'
     }
   ],
 
-  chromeDriver: 'node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_80.0.3987.106',
+  chromeDriver:
+    'node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_80.0.3987.106',
 
   //Add test scripts in this array to execute in parallel
   specs: ['tests/spec.js']

3f89c53672b3bcac6b196443204a38b252fd955c
Arthit Kanjai <arthit.kanjai@nxp.com>
2020-03-18 14:26:55 +0700

    save

--- conf.js
+++ conf.js
@@ -1,16 +1,17 @@
 exports.config = {
   // add more browsers to this array for more parallel tests!!
-  multiCapabilities: [
-    {
-      browserName: 'chrome',
-      chromeOptions: {
-        binary: './chromedriver.exe',
-        args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
-      }
+  capabilities: {
+    browserName: 'firefox',
+    marionette: true,
+    firefoxOptions: {
+      args: ['--headless']
+    },
+    'moz:firefoxOptions': {
+      args: ['--headless']
     }
-  ],
-
-  chromeDriver: './chromedriver',
+  },
+  geckoDriver: './geckodriver.exe',
+  directConnect: true,
 
   //Add test scripts in this array to execute in parallel
   specs: ['tests/spec.js']
